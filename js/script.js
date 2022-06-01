3678eab0c804888d871913bf0644fe7eb036cb81
Julien BOUCHET <julien.bouchet@efrei.net>
2022-06-01 13:19:53 +0200

    Co-authored-by: EgnaL20 <EgnaL20@users.noreply.github.com> Co-authored-by: Neptade <Neptade@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -2,16 +2,15 @@ var newCommandForm = document.forms.newTaskF;
 
 function start_end_task(){
   var d = new Date();
-  var date = document.createElement('td');
-  date.textContent = (d.getFullYear()+"-"+d.getMonth()+"-"+d.getDate()+" at "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds());
+  var date = new String;
+  date = (d.getFullYear()+"-"+d.getMonth()+"-"+d.getDate()+" at "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds());
   return date;
 }
 
 function incrementerDuree() {
-  let durees = document.getElementsByClassName("Duration (s)")
+  let durees = document.getElementsByClassName("duree")
   if (durees.length != 0) {
-    Array.prototype.forEach.call(durees,
-    function(dureeElement) {
+    Array.prototype.forEach.call(durees, function(dureeElement) {
       let valeur = parseInt(dureeElement.textContent)
       dureeElement.textContent = valeur + 1
     });
@@ -28,37 +27,54 @@ function ajouter(){
     const taskTd = document.createElement('td')
     const dateTd = document.createElement('td')
     const categorieTd = document.createElement('td')
+    const ajouterTd=document.createElement('td')
     const durationTd = document.createElement('td')
     taskTd.textContent = document.newTaskF.task.value
     dateTd.textContent = document.newTaskF.date.value
+    ajouterTd.textContent=start_end_task()  
     categorieTd.textContent = document.newTaskF.category.value
+    durationTd.classList.add('duree')
+    const finish = document.createElement("td")
+    const buttonFinish = document.createElement("button")
     //const selectEntree = document.getElementById("entreeId");
     //const valeurselectionnee = selectEntree.options[selectEntree.selectedIndex].value;
     //const textselectionne = selectEntree.options[selectEntree.selectedIndex].text;
-   //Vérification de la récupération
+    //Vérification de la récupération
     console.log(taskTd.textContent)
     console.log(dateTd.textContent)
     console.log(categorieTd.textContent)
     console.log(durationTd.textContent)
 
-    if (!document.newTaskF.task.checkValidity() ||
-          !document.newTaskF.date.checkValidity() ||
-          !document.newTaskF.category.checkValidity()
-         ) {
-               return
-        }
+    if (!document.newTaskF.task.checkValidity() || !document.newTaskF.date.checkValidity() || !document.newTaskF.category.checkValidity()){
+      return
+    }
+
+    let isFinish = false
+    buttonFinish.onclick = () => isFinish = true
+    buttonFinish.textContent = "Finish task"
+
     
+
     //const table = document.querySelector('table')
-    newItem.append(taskTd, dateTd, categorieTd, start_end_task(), setTimeout(durationTd, incrementerDuree()))
+    newItem.append(taskTd, dateTd, categorieTd, ajouterTd, durationTd, finish, buttonFinish)
+
+    if(!isFinish){
+      durationTd.textContent = setInterval("incrementerDuree()",1000)
+    }
+    else{
+      durationTd.textContent = "Finished"
+    }
+    
 
      /* le premier élément dans le document qui contient la classe "datatable" est retourné*/
     const table = document.querySelector('.datatable tbody')
     /*  Ex2)3)vi) */
     table.appendChild(newItem)
-        
+
+   
 }
 
- //supprimer toutes les lignes du tableau
+//supprimer toutes les lignes du tableau
 function supprimer() {
         const tbody = document.querySelector('.datatable tbody' )
         while (tbody.firstChild) {
@@ -67,29 +83,9 @@ function supprimer() {
     
 }
 
-function button(){
-  const buttonTd = document.createElement('td')
-}
-
-document.createElement("Finish task").addEventListener("click", "Finis")
-
-// variable to store our intervalID
-let nIntervId;
-
-function flashText() {
-  const oElem = document.getElementById("my_box");
-  if (oElem.className === "go") {
-    oElem.className = "stop";
-  } else {
-    oElem.className = "go";
-  }
-}
-
-function stopTextColor() {
-  clearInterval(nIntervId);
-  // release our intervalID from the variable
-  nIntervId = null;
-}
-
-document.getElementById("start").addEventListener("click", changeColor);
-document.getElementById("stop").addEventListener("click", stopTextColor);
+    
+    let isFinish = false
+    buttonFinish.onclick = () => isFinish = true
+    buttonFinish.textContent = "Finish task"
+    
+   
